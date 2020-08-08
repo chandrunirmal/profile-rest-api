b10d424e15ef4e42d082586596071d2394f3cc67
Chandru <33563555+chandrunirmal@users.noreply.github.com>
2020-08-08 18:39:57 +0530

    Added custom user profile model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles_api',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Overiding default user model from django with the created one at profile_api app in models
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
