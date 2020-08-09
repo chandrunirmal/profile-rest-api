5969e9621464e4be37aa722d112a4cfbdf8cf92b
Chandru <33563555+chandrunirmal@users.noreply.github.com>
2020-08-09 23:42:01 +0530

    Corrected static root typo

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOR = 'static/'
+STATIC_ROOT = 'static/'
 # Overiding default user model from django with the created one at profile_api app in models
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
